5dcf3e1cfd064f9e603af097042d723d53ed02df
Nelyubina Evgeniia <klimova.eug@yandex.ru>
2024-04-16 11:51:58 +0300

    Homework2:Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая на вход принимает натуральное число
+// N, а на выходе показывает его цифры через запятую.
+
+using System;
+class Program
+{
+    static void Main()
+    {
+        SeparateNumber();
+    }
+    static void SeparateNumber()
+    {
+        Console.WriteLine("Запишите число: ");
+        string someNum = Console.ReadLine();
+
+        if (int.TryParse(someNum, out int num))
+        {
+            foreach (char digit in someNum)
+            {
+                Console.Write(digit + ",");
+            }
+            Console.WriteLine();
+        }
+        else 
+        {
+            Console.WriteLine("Некорректно. Введите число.");
+            SeparateNumber();
+        }
+    }
+
+}    
